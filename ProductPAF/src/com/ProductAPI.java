9073260ea64bcf7cae74f2e50f016a272362cfe8
it19157306 <it19157306@my.sliit.lk>
2021-05-14 15:40:55 +0530

    add main method

--- ProductPAF/src/com/ProductAPI.java
+++ ProductPAF/src/com/ProductAPI.java
@@ -12,3 +12,29 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+/**
+* Servlet implementation class PaymentService
+*/
+@WebServlet("/ProductAPI")
+public class ProductAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      
+	product productObj = new product();
+   /**
+    * @see HttpServlet#HttpServlet()
+    */
+   public ProductAPI() {
+       super();
+       // TODO Auto-generated constructor stub
+   }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
