90ad81ef0afa7b2433b7f8205549144ecbea1dee
it19157306 <it19157306@my.sliit.lk>
2021-05-14 15:51:12 +0530

    add save button function

--- ProductPAF/WebContent/Components/product.js
+++ ProductPAF/WebContent/Components/product.js
@@ -6,4 +6,69 @@ $(document).ready(function()
 		$("#alertSuccess").hide();  
 	    $("#alertError").hide(); 
 }); 
+ // SAVE ============================================ 
+$(document).on("click", "#btnSave", function(event) 
+{  
+	// Clear alerts---------------------  
+	$("#alertSuccess").text("");  
+	$("#alertSuccess").hide();  
+	$("#alertError").text("");  
+	$("#alertError").hide(); 
  
+	// Form validation-------------------  
+	var status = validateProductForm();  
+	if (status != true)  
+	{   
+		$("#alertError").text(status);   
+		$("#alertError").show();   
+		return;  
+	} 
+ 
+	// If valid------------------------  
+	var type = ($("#hidProductIDSave").val() == "") ? "POST" : "PUT"; 
+
+	$.ajax( 
+	{  
+			url : "ProductAPI",  
+			type : type,  
+			data : $("#formProduct").serialize(),  
+			dataType : "text",  
+			complete : function(response, status)  
+			{   
+				onProductSaveComplete(response.responseText, status);  
+			} 
+	}); 
+}); 
+
+
+function onProductSaveComplete(response, status) 
+{  
+	if (status == "success")  
+	{   
+		var resultSet = JSON.parse(response); 
+
+		if (resultSet.status.trim() == "success")   
+		{    
+			$("#alertSuccess").text("Successfully saved.");    
+			$("#alertSuccess").show(); 
+
+			$("#divProductGrid").html(resultSet.data);   
+		} else if (resultSet.status.trim() == "error")   
+		{    
+			$("#alertError").text(resultSet.data);    
+			$("#alertError").show();   
+		} 
+
+	} else if (status == "error")  
+	{   
+		$("#alertError").text("Error while saving.");   
+		$("#alertError").show();  
+	} else  
+	{   
+		$("#alertError").text("Unknown error while saving..");   
+		$("#alertError").show();  
+	} 
+
+	$("#hidProductIDSave").val("");  
+	$("#formProduct")[0].reset(); 
+} 
